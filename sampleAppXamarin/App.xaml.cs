3ab7fe9cac61e2b965a2056a230779f0d8068ba8
Chris Young <cyoung@architectnow.net>
2018-04-18 08:51:20 -0400

    Changes

--- sampleAppXamarin/App.xaml.cs
+++ sampleAppXamarin/App.xaml.cs
@@ -2,6 +2,7 @@
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Push;
 
 namespace sampleAppXamarin
 {
@@ -11,7 +12,7 @@ namespace sampleAppXamarin
         {
             AppCenter.Start("android=804d0ed1-cab0-4feb-85fa-03b51e435baf;" +
                             "ios=0bf65012-5284-4fc6-ae1a-23ae3ba4cc73;",
-                   typeof(Analytics), typeof(Crashes));
+                            typeof(Analytics), typeof(Crashes), typeof(Push));
             MainPage = new ButtonPage();
         }
     }
